e5f8c1641fac41228fd02f5e9a59e490b6408d4c
Sherlyrhmaa <168067974+Sherlyrhmaa@users.noreply.github.com>
2025-07-16 06:25:37 +0700

    Update database.js

--- UAS PBM/backend/config/database.js
+++ UAS PBM/backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'najwa',         // sesuaikan dengan DB kamu
+  password: 'sherly',         // sesuaikan dengan DB kamu
   database: 'uas_pbm' // sesuaikan juga
 });
 
